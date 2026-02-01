362601f91e45e28c904487adc959ba1826b9dc57
Arijit Das <arijitdas.das99@gmail.com>
2026-02-01 12:39:34 +0530

    Encapsulation

--- Encapsulation/Example1.py
+++ Encapsulation/Example1.py
@@ -3,3 +3,31 @@ Docstring for Encapsulation.Example1
 
 Encapsulation means wrapping data and functions into single unit (object)
 """
+
+class Account:
+
+    def __init__(self, balance, acc_no):
+        
+        self.balance = balance
+        self.acc_no = acc_no
+
+    def debit(self, amount):
+
+        if self.balance > 0:
+            self.balance -= amount
+        print("Total balance : ",self.showBalance())
+
+    def credit(self, amount):
+
+        self.balance += amount
+        print("Total balance : ",self.showBalance)
+
+    def showBalance(self):
+
+        print(f"Account number : {self.acc_no}")
+        print(f"Available balance is : {self.balance}")
+
+a = Account(10000, 1234)
+a.debit(5000)
+a.credit(1000)
+a.showBalance()
