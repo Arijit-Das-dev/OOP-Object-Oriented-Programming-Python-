3255db0980f5fc1a2a005a66c0ff054d7718c9a5
Arijit Das <arijitdas.das99@gmail.com>
2026-02-01 10:46:56 +0530

    Deafult and parameterized cons

--- Constructors/Default&ParameterizedConstructor.py
+++ Constructors/Default&ParameterizedConstructor.py
@@ -1,3 +1,16 @@
 """
 Docstring for Constructors.Default&ParameterizedConstructor
 """
+
+class Example:
+
+    def __init__(self): # Default constructor
+        pass
+
+    def __init__(self, FirstName, LastName): # Parameterized constructor
+
+        self.FirstName = FirstName
+        self.LastName = LastName
+
+e = Example("Arijit", "Das")
+print(e.FirstName, e.LastName)
