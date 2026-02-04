7a9b85a1775931e4f225a93d0816f8c3ae6521cb
Arijit Das <arijitdas.das99@gmail.com>
2026-02-04 12:19:36 +0530

    new file added

---
+++ Encapsulation/Question3.py
@@ -0,0 +1,14 @@
+"""
+Docstring for Encapsulation.Question3
+
+Design an ATM class:
+
+private PIN
+
+private balance
+
+methods: authenticate, check_balance, withdraw
+
+block operations if PIN is wrong
+
+"""
