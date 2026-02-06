7108fccb3b0d7ef90f0b7051631b962f1d6b27b6
Arijit Das <arijitdas.das99@gmail.com>
2026-02-06 15:01:58 +0530

    updated

---
+++ Inheritance/Multi-Level-Inheritance/Question1.py
@@ -0,0 +1,25 @@
+"""
+Docstring for Inheritance.Multi-Level-Inheritance.Question1
+
+structure:
+[Grandparent => Parent => Child]
+
+Syntax:
+{
+class GrandParent:
+
+    def method1(self):
+        pass
+    
+class Parent(GrandParent):
+
+    def method2(self):
+        pass
+        
+class Child(Parent):
+
+    def method3(self):
+        pass
+}   
+"""
+
