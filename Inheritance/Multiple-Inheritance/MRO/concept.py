9833da2c3bb5cf8a893f7a29f1bcbb3686967bdc
Arijit Das <arijitdas.das99@gmail.com>
2026-02-07 10:42:21 +0530

    new folder and file added with new concept [MRO]

---
+++ Inheritance/Multiple-Inheritance/MRO/concept.py
@@ -0,0 +1,7 @@
+"""
+Docstring for Inheritance.Multiple-Inheritance.MRO.concept
+
+MRO = Method Resolution Order
+
+order in which Python searches methods
+"""
---
+++ Inheritance/Multiple-Inheritance/Question2.py
@@ -0,0 +1,3 @@
+"""
+Docstring for Inheritance.Multiple-Inheritance.Question2
+"""
