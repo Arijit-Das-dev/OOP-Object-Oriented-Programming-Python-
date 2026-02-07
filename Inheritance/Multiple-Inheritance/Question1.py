617c39d2976e9913b0146321f6c1d8f1f6625072
Arijit Das <arijitdas.das99@gmail.com>
2026-02-07 10:32:47 +0530

    updated

--- Inheritance/Multiple-Inheritance/Question1.py
+++ Inheritance/Multiple-Inheritance/Question1.py
@@ -1,3 +1,6 @@
 """
 Docstring for Inheritance.Multiple-Inheritance.Question1
+
+deff : Where one child class inherits from more than one parent class then it is called Multiple-Inheritance.
+       In multiple inheritance parent classes does not need to inherit each other always.
 """
