05e6fbfe1729724382a7ca1b5147380e7359e1da
Arijit Das <arijitdas.das99@gmail.com>
2026-02-07 10:39:09 +0530

    updated

--- Inheritance/Multiple-Inheritance/Question1.py
+++ Inheritance/Multiple-Inheritance/Question1.py
@@ -16,3 +16,24 @@ class Child(Parent1, Parent2):
     pass
 } 
 """
+
+# In that case child class can perform both the operations which belongs to parent 1 and 2 alongside itself
+class Parent1:
+
+    def quality1(self):
+        print("Parent 1 can sing")
+
+class Parent2:
+    
+    def quality2(self):
+        print("Parent 2 can dance")
+
+class Child(Parent1, Parent2):
+
+    def quality3(self):
+        print("Child can perform both")
+
+c = Child()
+c.quality1()
+c.quality2()
+c.quality3()
